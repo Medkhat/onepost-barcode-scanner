2bee54bb21e54b4c7a897af44d34dfe087b5d3e8
Medkhat <dev.medkhat@gmail.com>
2024-07-09 20:09:03 +0500

    This commit adds: [organizations pagination]

--- src/shared/components/table/data-table-row-actions.tsx
+++ src/shared/components/table/data-table-row-actions.tsx
@@ -25,7 +25,7 @@ export function DataTableRowActions<TData extends { id?: string }>(
           className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
         >
           <EllipsisIcon className="h-4 w-4" />
-          <span className="sr-only">Open menu</span>
+          <span className="sr-only">{props.row.original.id}</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[160px]">
