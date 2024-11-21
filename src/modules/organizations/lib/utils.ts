bfdc243f7f52b5bf6cfe07f26080df7c5d55946e
Medkhat <dev.medkhat@gmail.com>
2024-11-21 16:54:38 +0500

    Dashboard data; Charts

--- src/modules/organizations/lib/utils.ts
+++ src/modules/organizations/lib/utils.ts
@@ -2,16 +2,16 @@ import { WorkingHour } from "@/modules/organizations/api/organizations.types"
 
 export const isOrgAlwaysOpen = (workingHours: WorkingHour[]): boolean => {
   return workingHours.every(
-    (wh) => wh.open_time === "00:00" && wh.close_time === "23:59"
+    (wh) => wh.start_time === "00:00" && wh.end_time === "23:59"
   )
 }
 
 export const isOrgWorkTimeSame = (workingHours: WorkingHour[]): boolean => {
   return workingHours.every(
     (wh) =>
-      wh.open_time !== "00:00" &&
-      wh.close_time !== "23:59" &&
-      wh.open_time === workingHours[0].open_time &&
-      wh.close_time === workingHours[0].close_time
+      wh.start_time !== "00:00" &&
+      wh.end_time !== "23:59" &&
+      wh.start_time === workingHours[0].start_time &&
+      wh.end_time === workingHours[0].end_time
   )
 }
